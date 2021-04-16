fb48925fd24059b6559d183084fb2ae1e52c3e04
shin1201 <shin1201@next-code.com>
2021-04-16 16:08:22 +0900

    commit List

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -1,7 +1,8 @@
 package com.web.controller;
 
+import com.web.domain.User;
 import com.web.service.BoardService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -10,25 +11,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    private final BoardService boardService;
 
-    @Autowired
-    BoardService boardService;
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
     @GetMapping({"", "/"})
-    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-        return "/board/form";
+        return "board/form";
     }
 
     @GetMapping("/list")
-    public String list(@PageableDefault Pageable pageable, Model model){
+    public String list(@PageableDefault Pageable pageable, User user, Model model) {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
-        return "/board/list";
+        return "board/list";
     }
-
 
 }
