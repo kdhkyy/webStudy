d26c3181f76691cff0e62480d5bec90922bf462c
shin1201 <shin1201@next-code.com>
2021-04-16 15:44:00 +0900

    commit

--- src/test/java/com/web/JpaMappingTests.java
+++ src/test/java/com/web/JpaMappingTests.java
@@ -3,9 +3,10 @@ package com.web;
 import com.web.domain.Board;
 import com.web.domain.User;
 import com.web.domain.enums.BoardType;
+
+
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +19,9 @@ import java.time.LocalDateTime;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@DataJpaTest
+@DataJpaTest()
 @RunWith(SpringRunner.class)
-public class JpaMappingTest {
+public class JpaMappingTests {
     private final String boardTestTitle = "테스트123";
     private final String email = "test@gmail.com";
 
@@ -50,7 +51,7 @@ public class JpaMappingTest {
     }
 
     @Test
-    public void 제대로_생성됬는지_테스트(){
+    public void tests(){
         User user = userRepository.findByEmail(email);
         assertThat(user.getName(), is("havi"));
         assertThat(user.getPassword(), is("1234"));
@@ -59,7 +60,7 @@ public class JpaMappingTest {
         Board board = boardRepository.findByUser(user);
         assertThat(board.getTitle(), is(boardTestTitle));
         assertThat(board.getSubTitle(), is("서브 타이틀"));
-        assertThat(board.getContnet(), is("컨텐츠"));
+        assertThat(board.getContnet(), is("콘텐츠"));
         assertThat(board.getBoardType(), is(BoardType.free));
     }
 }
